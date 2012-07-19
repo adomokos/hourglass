380d3b5b851607f05b1275a58fb18c45f9b5b39e
Nate Hoellein <natehoellein@gmail.com>
2012-07-19 13:47:11 -0400

    added array compare function for finding winners

--- spec/finds_winner_spec.js
+++ spec/finds_winner_spec.js
@@ -19,6 +19,7 @@ describe('FindsWinner', function() {
      });
     });
   });
+
   it('when A wins', function() {
     var that = this;
     this.board.pick(this.players, 'A1');
@@ -33,5 +34,22 @@ describe('FindsWinner', function() {
       });
     });
     
+  });
+
+  it('when B wins', function() {
+    var that = this;
+    this.board.pick(this.players, 'A1');
+    this.board.pick(this.players, 'B1');
+    this.board.pick(this.players, 'A2');
+    this.board.pick(this.players, 'B2');
+    this.board.pick(this.players, 'C1');
+    this.board.pick(this.players, 'B3', function(snapshot) {
+      that.findsWinner.didIWin(snapshot, function(win) {
+        win.won.should.be.true;
+        win.player.should.equal('Declan');
+        win.slots.should.eql(['B1', 'B2', 'B3']);
+      });
+    });
+
   });
 });
