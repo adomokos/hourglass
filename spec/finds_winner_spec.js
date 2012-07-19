8724ffb729c565cdbf8da35975a8c40eba1d96c0
Nate Hoellein <natehoellein@gmail.com>
2012-07-19 14:57:58 -0400

    fixed tests and code around callback not called and asserts not being run

--- spec/finds_winner_spec.js
+++ spec/finds_winner_spec.js
@@ -10,17 +10,18 @@ describe('FindsWinner', function() {
     this.board = new Board(this.players);
   });
 
-  it('with 1 move I did not win', function() {
+  it('with 1 move I did not win', function(done) {
     var that = this;
     this.board.pick(this.players, 'A1', function(snapshot){
       snapshot.A.should.eql(['A1']);
       that.findsWinner.didIWin(snapshot, function(win) {
         win.won.should.be.false;
+        done();
      });
     });
   });
 
-  it('when A wins', function() {
+  it('when A wins, first row', function(done) {
     var that = this;
     this.board.pick(this.players, 'A1');
     this.board.pick(this.players, 'B1');
@@ -31,12 +32,13 @@ describe('FindsWinner', function() {
         win.won.should.be.true;
         win.player.should.equal('Reilly');
         win.slots.should.eql(['A1', 'A2', 'A3']);
+        done();
       });
     });
     
   });
 
-  it('when B wins', function() {
+  it('when B wins, second row', function(done) {
     var that = this;
     this.board.pick(this.players, 'A1');
     this.board.pick(this.players, 'B1');
@@ -48,8 +50,42 @@ describe('FindsWinner', function() {
         win.won.should.be.true;
         win.player.should.equal('Declan');
         win.slots.should.eql(['B1', 'B2', 'B3']);
+        done();
       });
     });
-
   });
+
+  it('when B wins, third row', function(done) {
+    var that = this;
+    this.board.pick(this.players, 'A1');
+    this.board.pick(this.players, 'C1');
+    this.board.pick(this.players, 'A2');
+    this.board.pick(this.players, 'C2');
+    this.board.pick(this.players, 'B1');
+    this.board.pick(this.players, 'C3', function(snapshot) {
+      that.findsWinner.didIWin(snapshot, function(win) {
+        win.won.should.be.true;
+        win.player.should.equal('Declan');
+        win.slots.should.eql(['C1', 'C2', 'C3']);
+        done();
+      });
+    });
+  });
+
+  it('when A wins, first column', function(done) {
+    var that = this;
+    this.board.pick(this.players, 'A1');
+    this.board.pick(this.players, 'C2');
+    this.board.pick(this.players, 'B1');
+    this.board.pick(this.players, 'C3');
+    this.board.pick(this.players, 'C1', function(snapshot) {
+      that.findsWinner.didIWin(snapshot, function(win) {
+        win.won.should.be.true;
+        win.player.should.equal('Reilly');
+        win.slots.should.eql(['A1', 'B1', 'C1']);
+        done();
+      });
+    });
+  });
+
 });
